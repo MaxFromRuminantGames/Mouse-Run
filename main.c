7240ddb119790fb39a744a20de73ff93ca51c109
MaxFromRuminantGames <epic_couzinz@outlook.com>
2024-06-08 13:34:11 -0700

    restarting from scratch cause my code sucks

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define max(a,b) (a>b ? a : b)
+#define min(a,b) (a<b ? a : b)
+#define clamp(x,lo,hi) (min( hi, max(lo,x) ))
 
 int main()
 {
-	printf("Wazzup");
+
 }
